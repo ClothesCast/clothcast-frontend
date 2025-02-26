5520275d75167eddfb9f33f8103cdccb8677e7e1
hyemimi <hyemi7375@gmail.com>
2025-02-26 17:55:58 +0900

    feat: survey page

---
+++ src/components/common/PageContainer/index.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const PageContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: calc(100vh - 40px);
+  max-width: 600px;
+  margin: 0 auto;
+  padding: 0.2rem;
+`;
+
+export default PageContainer
